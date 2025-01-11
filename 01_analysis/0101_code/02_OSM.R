446207d6c257b6500e3a08e3a418b2ef6c2ccd68
STBrinkmann <bastibrinkmann94@gmail.com>
2025-01-11 22:37:07 +0100

    data: include generall greenspace

--- 01_analysis/0101_code/02_OSM.R
+++ 01_analysis/0101_code/02_OSM.R
@@ -74,3 +74,53 @@ parks_sf <- parks_sf %>%
 
 # Save
 write_sf(parks_sf, "01_analysis/0102_data/02_processed/02_Nbg_Parks.gpkg")
+
+
+#'
+#' # 3. OSM - Broader Greenspace
+#'
+
+# 1 Prepare bounding box (expand AOI by 5 km, for instance)
+bb <- aoi %>%
+  st_buffer(5000) %>%
+  st_bbox()
+
+# 2 Query OSM for relevant green features + accessibility
+q_greenspace <- opq(bbox = bb, timeout = 1000) %>%
+  # OR-logic for multiple key-value pairs
+  add_osm_features(
+    features = c(
+      'leisure="park"',
+      'leisure="recreation_ground"',
+      'boundary="nature_reserve"',
+      'landuse="forest"',
+      'landuse="meadow"',
+      'leisure="garden"',
+      'leisure="playground"',
+      'amenity="community_centre"'
+    )
+  ) %>%
+  osmdata_sf()
+
+# 3 Intersect results with AOI, combine polygons and multipolygons
+greenspace_poly <- st_intersection(q_greenspace$osm_polygons, aoi)
+greenspace_mpoly <- st_intersection(q_greenspace$osm_multipolygons, aoi)
+
+greenspace_sf <- rbind(
+  greenspace_poly  %>% select(access, geometry),
+  greenspace_mpoly %>% select(access, geometry)
+) %>%
+  filter(!access %in% c("customers", "no", "permissive", "private")) %>%
+  select(-access)
+
+# 4 Reproject & filter by size (> 1 ha), then remove duplicates
+greenspace_sf <- greenspace_sf %>%
+  st_transform(25832) %>%
+  mutate(area = st_area(geometry)) %>%
+  filter(area > units::set_units(1, "ha")) %>%
+  select(geometry) %>%
+  distinct()
+
+# Save
+write_sf(greenspace_sf, "01_analysis/0102_data/02_processed/02_Nbg_Greenspace.gpkg")
+
